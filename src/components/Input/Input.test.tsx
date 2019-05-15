9c691d106e68250a304b7ae4a3028bfe23f033af
Philipp Bauch <philipp.bauch@kinexon.com>
2019-05-15 16:57:27 +0200

    [TEST] Input component test

---
+++ src/components/Input/Input.test.tsx
@@ -0,0 +1,13 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import sinon from 'sinon';
+import Input from './Input';
+
+describe('<Input />', () => {
+  it('renders a `div`', () => {
+    const handleChange = sinon.spy();
+    const value = '';
+    const wrapper = shallow(<Input onChange={handleChange} value={value} />);
+    expect(wrapper.contains('input')).toEqual(true);
+  });
+});
