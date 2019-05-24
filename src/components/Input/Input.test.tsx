3af77bdac5c536f0b4697eaeb50db0d3b8fc02fd
Philipp Bauch <philipp.bauch@kinexon.com>
2019-05-24 11:49:48 +0200

    [FIX] Test import

--- src/components/Input/Input.test.tsx
+++ src/components/Input/Input.test.tsx
@@ -4,7 +4,7 @@ import sinon from 'sinon';
 import Input from './Input';
 
 describe('<Input />', () => {
-  it('renders a `div`', () => {
+  it('renders an `input`', () => {
     const handleChange = sinon.spy();
     const value = '';
     const wrapper = shallow(<Input onChange={handleChange} value={value} />);
--- src/core/App/App.test.tsx
+++ src/core/App/App.test.tsx
@@ -1,4 +1,4 @@
-import PrivateRoute from '@/components/PrivateRoute';
+import PrivateRoute from '@/core/PrivateRoute';
 import { Login } from '@/pages';
 import React from 'react';
 import ReactDOM from 'react-dom';
